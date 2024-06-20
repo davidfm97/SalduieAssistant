10614c11d7274cd8f19974e068b7e27796548b3a
DFuentes <64693066+davidfm97@users.noreply.github.com>
2024-06-20 13:19:19 +0200

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
+  // Lee el contenido del archivo README.md
+  fs.readFile('README.md', 'utf8', function(err, data) {
+    if (err) {
+      console.error("Error al leer el archivo README.md:", err);
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.write('Error interno del servidor');
+      return res.end();
+    }
+
+    // Envía el contenido del README.md como respuesta
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(data);
+    res.end();
+  });
 }).listen(8080);
+
+console.log('Servidor HTTP iniciado en el puerto 8080');
