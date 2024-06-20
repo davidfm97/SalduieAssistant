5c9085d00ca7dd7f25d4456fb4d3e00727cdda1c
DFuentes <64693066+davidfm97@users.noreply.github.com>
2024-06-20 14:03:24 +0200

    Update keep_alive.js

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -1,24 +1,62 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
+
+const guiaContent = `
+# Salduie Assistant BOT
+## Banco de semillas en discord
+
+Este bot de Discord permite gestionar un banco de semillas en una hoja de cálculo de Google Sheets. Los usuarios pueden añadir semillas y listar las semillas existentes con diversas opciones de filtrado.
+
+## Comandos
+
+### \`/add\`
+
+Añade una entrada al banco de semillas.
+
+#### Parámetros
+
+- \`user\` (Usuario, requerido): El usuario que añade la semilla.
+- \`seed\` (Semilla, requerido): La semilla a añadir.
+- \`variety\` (Variedad, requerido): La variedad de la semilla.
+- \`scientific_name\` (Nombre científico, requerido): El nombre científico de la planta.
+- \`harvest_year\` (Año de recolección, requerido): El año de recolección.
+- \`location\` (Lugar de recolección, opcional): El lugar de recolección.
+- \`observations\` (Observaciones, opcional): Observaciones adicionales.
+- \`image_url\` (URL de la imagen, opcional): URL de la imagen de la planta.
+
+#### Ejemplo de Uso
+/add user: @Usuario seed: Tomate variety: Cherry scientific_name: Solanum lycopersicum harvest_year: 2023 location: Mi Jardín observations: Muy dulce image_url: https://example.com/image.jpg
+
+### \`/list\`
+
+Muestra todos los usuarios agregados a la lista de semillas con opción de filtrado.
+
+#### Parámetros
+
+- \`filter\` (Filtro, opcional): Filtra los resultados por un criterio específico.
+
+#### Ejemplo de Uso
+/list filter: Tomate
+
+## Ejemplo de Datos en la Hoja de Cálculo
+
+A continuación, se muestra un ejemplo de cómo se estructuran los datos en la hoja de cálculo de Google Sheets:
+
+| Usuario            | Semilla   | Variedad              | Nombre científico       | Año de recolección | Lugar de recolección | Observaciones | Imágenes                                                                 |
+|--------------------|-----------|-----------------------|-------------------------|--------------------|----------------------|---------------|-------------------------------------------------------------------------|
+| usuario1           | Pimientos | -                     | Capsicum annuum         | 2023               |                      | Variante dulce |                                                                         |
+| usuario2           | Tomate    | Cherry Pera Amarillo  | Solanum lycopersicum    | 2024               |                      |               | https://cdn.discordapp.com/attachments/.../5.tomateCherryPeraAmarillo.jpg |
+
+## Notas Adicionales
+
+- Los usuarios pueden añadir múltiples entradas, incluso si ya han añadido una entrada previamente.
+- Se puede utilizar el comando \`/list\` para filtrar las semillas por cualquier campo disponible.
+- Asegúrate de proporcionar una URL válida para la imagen si decides incluir una.
+`;
 
 const server = http.createServer(function (req, res) {
-  // Construye la ruta completa al archivo guia.md usando __dirname
- const filePath='/opt/render/project/src/guia.md'
-  // Lee el contenido del archivo guia.md
-  fs.readFile(filePath, 'utf8', function(err, data) {
-    if (err) {
-      console.error("Error al leer el archivo guia.md:", err);
-      res.writeHead(500, {'Content-Type': 'text/html'});
-      res.write('Error interno del servidor');
-      return res.end();
-    }
-
-    // Envía el contenido del guia.md como respuesta
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(guiaContent);
+  res.end();
 });
 
 server.listen(8080, () => {
