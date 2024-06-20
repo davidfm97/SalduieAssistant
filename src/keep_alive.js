335e8f2a4c47c2424cbe80d0b49f8540b6045e93
DFuentes <64693066+davidfm97@users.noreply.github.com>
2024-06-20 13:27:06 +0200

    Update keep_alive.js

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   // Lee el contenido del archivo README.md
-  fs.readFile('../README.md', 'utf8', function(err, data) {
+  fs.readFile('guia.md', 'utf8', function(err, data) {
     if (err) {
-      console.error("Error al leer el archivo README.md:", err);
+      console.error("Error al leer el archivo guia.md:", err);
       res.writeHead(500, {'Content-Type': 'text/html'});
       res.write('Error interno del servidor');
       return res.end();
